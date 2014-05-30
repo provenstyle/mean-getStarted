913ebbe96d6bb977b34b03097c6c7badc9393fc9
Michael Dudley <michaeldudley@michaels-mbp-2.home>
2014-05-30 06:25:01 -0500

    Refactoring the server code

--- server.js
+++ server.js
@@ -1,47 +1,16 @@
-var express = require('express'),
-    stylus = require('stylus'),
-    mongoose = require('mongoose');
+var express = require('express');
 
 var env = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-function compile(str, path){
-    return stylus(str).set('filename', path);
-};
+var config = require('./server/config/config')[env];
 
-app.configure(function(){
-    app.set('views', __dirname + '/server/views');
-    app.set('view engine', 'jade');
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(stylus.middleware({
-        src: __dirname + '/public',
-        compile: compile
-    }));
-    app.use(express.static(__dirname + '/public'));
-});
+require('./server/config/express')(app, config);
 
-if(env === 'development'){
-    mongoose.connect('mongodb://localhost/multivision');
-} else{
-    mongoose.connect('mongodb://multivisionUser:multivision@ds053168.mongolab.com:53168/multivision');
-}
+require('./server/config/mongoose')(config);
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error...'));
-db.once('open', function callback(){
-    console.log('multivision db opened');
-});
+require('./server/config/routes')(app);
 
-app.get('/partials/*', function(req, res){
-    res.render('../../public/app/' + req.params);
-});
-
-app.get('*', function(req, res){
-    res.render('index');
-});
-
-var port = process.env.PORT || 3030;
-app.listen(port);
-console.log('Listening on port ' + port + '...');
+app.listen(config.port);
+console.log('Listening on port ' + config.port + '...');
